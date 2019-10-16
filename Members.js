872b2476a2f0e10db876febf9cb0571a3cc80767
BEPUL-CSE-RU <bepul.cse.ru@gmail.com>
2019-10-16 21:49:53 +0600

    nodejs_app_(rest_api)
    node js app / rest api when watching crash video
    https://www.youtube.com/watch?reload=9&v=L72fhGm1tfE

---
+++ Members.js
@@ -0,0 +1,12 @@
+const members = [
+    {
+        id: 1,
+        name: 'rakib',
+        email: 'ra@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'arup'
+    }
+];
+module.exports = members;
